cb5401da19058acaf21a1b7b881367389baa92d9
Kağan Değirmen <kagandegirmen@gmail.com>
2024-05-23 20:49:52 +0300

    Highlights implemented

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,37 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+
 const Highlights = () => {
-  return <div>Highlights</div>;
+
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.4 });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img className="ml-2" src={watchImg} alt="watch" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img className="ml-2" src={rightImg} alt="right" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
